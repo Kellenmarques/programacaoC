93ecf546e69e0e698b7cd1bd913a34cbacc6ed1e
kellenmarques <kellen.ms16@gmail.com>
2022-05-03 13:47:50 -0300

    exercicio 02 resolvido

---
+++ ex02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+////programa pra ler numeros real e imprimi-los
+int main(){
+    float quilos;
+    printf ("insira seu peso: ");
+    scanf("%f", &quilos);
+    printf("quilos %f",quilos);
+}
