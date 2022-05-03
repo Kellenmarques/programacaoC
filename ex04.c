c2c0d913e81403e23c3bfdf29d74d060bfcb120d
kellenmarques <kellen.ms16@gmail.com>
2022-05-03 15:50:39 -0300

    exercicio 04 resolvido

---
+++ ex04.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float numberreal, square;
+    printf("numero: ");
+    scanf("%f",&numberreal);
+    // in this field I'm declaring the square variable 
+    square = numberreal * numberreal; 
+    printf("tottaly %f",square);
+}
