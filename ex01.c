ae2527de8ba74a1eeb517ed4d447bf2889082a9b
kellenmarques <kellen.ms16@gmail.com>
2022-05-03 13:44:01 -0300

    exercicio 01 resolvido

---
+++ ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//programa pra ler numeros inteiros e imprimi-los
+int main (){
+    int idade;
+    printf ("digite um valor: ");
+    scanf("%d",&idade);
+    printf("idade %d",idade);
+}
+
