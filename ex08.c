d71e20b3bf15548e6962a736ea79b250207ea622
kellenmarques <kellen.ms16@gmail.com>
2022-05-03 17:24:48 -0300

    exercicio 08 resolvido

---
+++ ex08.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float kelvin, celsius;
+    printf("graus kelvin: ");
+    scanf("%f", &kelvin);
+    celsius = kelvin - 273,13;
+    printf("totally %f",celsius);
+}
