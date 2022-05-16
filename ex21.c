2d6456f83f15111aceabcb8fd11596a9b28c75a5
kellenmarques <kellen.ms16@gmail.com>
2022-05-16 12:42:57 -0300

    exercicio 21 resolvido

---
+++ ex21.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float libras, quilogramas;
+    printf("libras: ");
+    scanf("%f", &libras);
+    quilogramas = libras*0.45;
+    printf("totally %f", quilogramas);
+}
